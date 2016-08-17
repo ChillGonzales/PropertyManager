d201bb7abb574e7fe5d459c8f168549e7b0d9e71
Colin Monroe <cmmonroe2010@gmail.com>
2016-08-17 14:54:05 -0400

    not sure why these weren't added

--- PropertyManager.WebUI/Controllers/ManagementController.cs
+++ PropertyManager.WebUI/Controllers/ManagementController.cs
@@ -5,57 +5,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using PropertyManager.Domain.Abstract;
-using PropertyManager.Domain.Entities;
 using PropertyManager.Domain.Concrete;
 
 namespace PropertyManager.WebUI.Controllers
 {
     public class ManagementController : Controller
     {
-<<<<<<< HEAD
         private IWaitList repository;
 
         public ManagementController(IWaitList repo)
         {
             this.repository = repo;
         }
-=======
-        private IUserRepository repository;
 
-        public ManagementController(IUserRepository repo)
-        {
-            this.repository = repo;
-        }
-
->>>>>>> origin/master
         // GET: Management
         public ActionResult Index()
         {
             return View();
         }
 
-<<<<<<< HEAD
         public ActionResult CreateUser()
         {
             return View();
         }
 
+        [HttpGet]
+        public ActionResult AddToWaitList()
+        {
+            return View();
+        }
+
+        [HttpPost]
         public ActionResult AddToWaitList(WaitListItem waiter)
         {
             if (waiter != null)
             {
-                
+                repository.Add(waiter);
             }
-            return View();
+            return RedirectToAction("ViewWaitList", "Management");
         }
 
-        public ViewResult ViewWaitList()
+        public ActionResult ViewWaitList()
         {
             return View(repository.WaitList.OrderBy(x => x.SignUpTS));
         }
 
-=======
         public ActionResult CreateUser(User user)
         {
             if (user != null)
@@ -64,6 +58,5 @@ namespace PropertyManager.WebUI.Controllers
             }
             return View();
         }
->>>>>>> origin/master
     }
 }
